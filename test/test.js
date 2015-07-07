74bcbe6d1fe4ba9d724941c3a8129f38e70d1909
calidion <calidion@gmail.com>
2015-07-07 19:26:29 +0800

    updated for travis

--- test/test.js
+++ test/test.js
@@ -4,12 +4,20 @@ var ytx = require('../');
 
 describe('node-ytx node module', function () {
   it('can send a sms message to the phone', function (done) {
-    var config = require('./config');
-    ytx.init(config.config);
-
-
-    //Specify a phone number which your can test
-    var phone = config.phone;
+    var fs = require('fs');
+    var path = require('path');
+    var phone = null;
+    if (fs.existsSync(path.resolve(__dirname, './config.js'))) {
+      var config = require('./config');
+      ytx.init(config.config);
+      //Specify a phone number which your can test
+      phone = config.phone;
+    } else if (process.env.config && process.env.phone) {
+      ytx.init(JSON.parse(process.env.config));
+      phone = process.env.phone;
+    } else {
+      assert(false);
+    }
 
     ytx.smsTemplate(phone, [' 云通讯测试', "" + Math.round(Math.random() * 1000000) ], 1, function (error, data) {
       var json;
